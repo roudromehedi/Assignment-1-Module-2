6c123e801b6bae08d5651e6ce15c910985288f23
RoudroMehedi <roudro.m@gmail.com>
2023-07-08 16:56:01 +0200

    WIP

--- src/exercise-4.js
+++ src/exercise-4.js
@@ -4,3 +4,18 @@ const isEven = (number) => (number % 2 === 0 ? 'Even' : 'Odd'
 console.log(
   isEven(7)
 )
+
+/*
+Exercise 4:
+
+Write a function named isEven that takes in a number as a parameter and returns the string "Even" if the number is even,
+and "Odd" if the number is odd. Use a ternary operator instead of an if/else statement.
+
+    Example:
+
+        Input:
+
+            isEven(7);
+Output:
+
+    "Odd" */
